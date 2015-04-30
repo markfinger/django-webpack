d29b4121c55de7b0a86a620fa8924d6d314e1874
Maxim Poletaev <zenwalker2@gmail.com>
2015-04-30 12:03:14 +0300

    Added templatetag

---
+++ django_webpack/templatetags/webpack.py
@@ -0,0 +1,13 @@
+from django_webpack.compiler import webpack
+from django.utils.html import mark_safe
+from django_webpack import settings
+from django import template
+import os
+
+register = template.Library()
+
+
+@register.simple_tag
+def webpack_bundle(config_path=settings.CONFIG_PATH):
+    bundle = webpack(os.path.join(settings.BUNDLE_ROOT, config_path))
+    return bundle.render()
